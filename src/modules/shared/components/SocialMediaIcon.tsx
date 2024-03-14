4b59238e15c61f842295d5de3ed55cd79440b7de
wpslakshitha <lakshitha@leafycode.com>
2024-03-14 14:13:04 +0530

    feat: get all social media icon to one component 🤲🏻

---
+++ src/modules/shared/components/SocialMediaIcon.tsx
@@ -0,0 +1,21 @@
+import { Facebook, Github, Instagram } from "lucide-react";
+
+export function SocialMediaIcon() {
+  const iconClassName = "w-[19.81px] h-[19.81px]";
+
+  return (
+    <div>
+      <div className="flex items-center gap-1 md:justify-end">
+        <div className="rounded-full p-2 hover:bg-muted cursor-pointer">
+          <Instagram color="#000000" className={iconClassName} />
+        </div>
+        <div className="rounded-full p-2 hover:bg-muted cursor-pointer">
+          <Github color="#000000" className={iconClassName} />
+        </div>
+        <div className="rounded-full p-2 hover:bg-muted cursor-pointer">
+          <Facebook color="#000000" className={iconClassName} />
+        </div>
+      </div>
+    </div>
+  );
+}
