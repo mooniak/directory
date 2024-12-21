8e815cae4d5f4c7459aab652644a3db3edaed26a
Harsha <harshapradeep210@gmail.com>
2024-12-21 11:44:59 +0530

    feat: add pages

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { index, type RouteConfig } from '@react-router/dev/routes'
+import { index, route, type RouteConfig } from '@react-router/dev/routes'
 
-export default [index('routes/home.tsx')] satisfies RouteConfig
+export default [
+  index('routes/home.tsx'),
+  route('about', 'routes/about.tsx'),
+  route('contact', 'routes/contact.tsx'),
+  route('community', 'routes/community.tsx'),
+] satisfies RouteConfig
